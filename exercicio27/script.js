0353ddc3e0e77bd455957b546fcd5546be1be689
Henrique Ribeiro Rodrigues <henriquerrodrigues3@gmail.com>
2025-09-11 19:50:26 -0300

    exercicio 27 resolvido

--- exercicio27/script.js
+++ exercicio27/script.js
@@ -2,27 +2,30 @@
 Nível Intermediário
 Você está desenvolvendo um sistema de agenda. Crie umprograma que permita ao usuário adicionar eventos(nome, data e hora) à agenda e depois exiba os eventosordenados por data e hora. */
 
-class Agendar{
-  constructor(valor1, valor2, valor3){
-    this.nome = valor1;
-    this.data = valor2;
-    this.hora = valor3;
+const agenda = [];
+
+while(true){
+  const evento = {};
+
+  evento.nome = prompt("digite o nome do evento ou sair para encerrar: ");
+
+  if(evento.nome.toLowerCase() === "sair"){
+    break;
   }
-  exibeEvento(calendario) {
-    for(let i = 0; i<calendario.length; i++){
-      console.log("evento: "+ calendario[i] + "\n");
-    }
-  }
+  evento.data = prompt("Digite a data no formato DD/MM/AAAA: ");
+  evento.hora = prompt("Digite a hora no formato HH:MM: ");
+
+  agenda.push(evento);
 }
 
-let reserva = 0;
-let data = 0;
-let hora = 0;
-while(reserva !=""){
-  reserva = prompt("digite o nome evento para a agenda: ");
-  
+agenda.sort((evento1, evento2) => {
+  const dataHora1 = new Date(`${evento1.data} ${evento1.hora}`);
+  const dataHora2 = new Date(`${evento2.data} ${evento2.hora}`);
+  return datahora1 - dataHora2;
+})
+
+//exibe os eventos em ordem
+alert("Eventos na agenda: ");
+for(const evento of agenda){
+  alert(`Evento: ${evento.nome}\nData: ${evento.data}\nHora ${evento.hora}`);
 }
-
-
-
-
